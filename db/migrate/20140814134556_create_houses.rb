85a4141c1537bdecbc1b167d28501c985ff8e26b
Matt Worth <matt.worth@gmail.com>
2014-08-14 14:52:39 +0100

    setup db for house model and migrate

--- db/migrate/20140814134556_create_houses.rb
+++ db/migrate/20140814134556_create_houses.rb
@@ -1,8 +1,19 @@
 class CreateHouses < ActiveRecord::Migration
   def change
     create_table :houses do |t|
+    	t. string :title
+    	t.string :address
+    	t.float :latitude
+    	t.float :longitude
+    	t.integer :price_in_pence
+    	t.intefer :numner_of_roooms
+		t.integer :max_guests
+    	t.boolean :is_featured
+    	t.boolean :available
 
-      t.timestamps
+		t.integer :user_id
+
+      	t.timestamps
     end
   end
 end
